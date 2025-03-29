75b862e9ff5605ff00aa84d159744b4070ac49a3
Christa Hegedus <christa@MacBookPro.home.local>
2025-03-28 20:51:59 -0400

    refactored record types for recipebdge

--- src/app/components/recipeDifficultyBadge.tsx
+++ src/app/components/recipeDifficultyBadge.tsx
@@ -1,31 +1,48 @@
+import { CreateBadgeMap } from './createBadge';
+
 export enum RecipeDifficulty {
   Easy = 'easy',
   Medium = 'medium',
   Hard = 'hard',
 }
 
-const RecipeDifficultyMap: Record<RecipeDifficulty, string> = {
-  [RecipeDifficulty.Easy]: 'Easy Peasy 🍋',
-  [RecipeDifficulty.Medium]: 'Intermediate 🧑‍🍳',
-  [RecipeDifficulty.Hard]: 'Expert Only 🔥',
-};
+// const RecipeDifficultyMap: Record<RecipeDifficulty, string> = {
+//   [RecipeDifficulty.Easy]: 'Easy Peasy 🍋',
+//   [RecipeDifficulty.Medium]: 'Intermediate 🧑‍🍳',
+//   [RecipeDifficulty.Hard]: 'Expert Only 🔥',
+// };
 
-const RecipeDifficultyColor: Record<RecipeDifficulty, string> = {
-  [RecipeDifficulty.Easy]: 'oklch(0.627 0.194 149.214)',
-  [RecipeDifficulty.Medium]: 'oklch(0.705 0.213 47.604)',
-  [RecipeDifficulty.Hard]: 'oklch(0.637 0.237 25.331)',
-};
+// const RecipeDifficultyColor: Record<RecipeDifficulty, string> = {
+//   [RecipeDifficulty.Easy]: 'oklch(0.627 0.194 149.214)',
+//   [RecipeDifficulty.Medium]: 'oklch(0.705 0.213 47.604)',
+//   [RecipeDifficulty.Hard]: 'oklch(0.637 0.237 25.331)',
+// };
+
+//refactored/combined badge component record types for recipes
+const RecipeBadge = CreateBadgeMap<RecipeDifficulty>({
+  [RecipeDifficulty.Easy]: {
+    label: 'Easy Peasy 🍋',
+    color: 'oklch(0.627 0.194 149.214)',
+  },
+  [RecipeDifficulty.Medium]: {
+    label: 'Intermediate 🧑‍🍳',
+    color: 'oklch(0.705 0.213 47.604)',
+  },
+  [RecipeDifficulty.Hard]: {
+    label: 'Expert Only 🔥',
+    color: 'oklch(0.637 0.237 25.331)',
+  },
+});
 
 export default function RecipeDifficultyBadge({
   difficulty,
 }: {
   difficulty: RecipeDifficulty;
 }) {
+  const { label, color } = RecipeBadge[difficulty];
   return (
     <>
-      <span style={{ color: RecipeDifficultyColor[difficulty] }}>
-        {RecipeDifficultyMap[difficulty]}
-      </span>
+      <span style={{ color }}>{label}</span>
     </>
   );
 }
