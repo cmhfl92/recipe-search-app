5b77acc17059c8ab08da42696fc3a6e9c867d459
Christa <christa.hegedus1@gmail.com>
2025-04-07 18:58:58 -0400

    connected my server file to MongoDB and installed mongoose

--- server/server.js
+++ server/server.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = 5000;
 
 app.use(cors());
 app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/recipesdb', {
+  useNewURLParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB!');
+});
+
+const Recipe = mongoose.model('Recipe', {
+  label: String,
+});
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Recipe API!');
