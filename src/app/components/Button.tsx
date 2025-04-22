7ca7eae2e9c05e14d7e92a70d92ff44f349c07a7
Christa <christa.hegedus1@gmail.com>
2025-04-21 23:11:55 -0400

    fixed delete button placement on cards and hover feature on buttons

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -5,17 +5,27 @@ interface ButtonProps {
   label: string;
   onClick: () => void;
   variant?: 'primary' | 'secondary';
+  className?: string;
 }
 
-const Button = ({ label, onClick, variant = 'primary' }: ButtonProps) => {
+const Button = ({
+  label,
+  onClick,
+  variant = 'primary',
+  className,
+}: ButtonProps) => {
   return (
     <>
       <div
-        className={`text-white px-4 py-2 rounded  ${
-          variant === 'primary' ? 'bg-green-600' : 'bg-red-400'
+        className={`text-white px-4 py-2 rounded   ${
+          variant === 'primary'
+            ? 'bg-green-600 hover:bg-green-800'
+            : 'bg-red-500 hover:bg-red-700'
         }`}
       >
-        <button onClick={onClick}>{label}</button>
+        <button onClick={onClick} className=''>
+          {label}
+        </button>
       </div>
     </>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -127,15 +127,24 @@ export default function Homepage() {
             >
               {isFavorite(recipe._id) ? '❤️' : '💔'}
             </button>
-            <button
+            {/* <Button
               onClick={() => {
                 handleDeleteRecipe(recipe._id);
               }}
-              className='absolute top-2 right-8 text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2'
+              className='absolute top-1 right-8 text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2'
               title='Delete'
-            >
-              Delete
-            </button>
+            /> */}
+            <div className='w-1/6'>
+              <Button
+                label='Delete'
+                onClick={() => {
+                  handleDeleteRecipe(recipe._id);
+                }}
+                variant='secondary'
+                className=''
+              />
+            </div>
+
             <div className='p-4'>
               <Image
                 src={recipe.image!}
